c8e368c7223b9f40dcbff10daf09e4de77131165
Kevin Donoghue <kjdmath@gmail.com>
2019-09-30 12:37:20 -0700

    remote setup instructions

--- generate_simple_sample.py
+++ generate_simple_sample.py
@@ -1,3 +1,3 @@
 from data.generate_sample import generate_sample
 
-generate_sample(1000, 'simple_sample', 224, 224, 'halves_wholes', 'halves_wholes', 'none', 'none', 0, measure_length_choices=(16,), key_number_choices=(0,))
+generate_sample(1, 5, 'simple_sample', 224, 224, 'halves_wholes', 'halves_wholes', 'none', 'none', 0, measure_length_choices=(16,), key_number_choices=(0,))
